7cee03c9b9821484e100124e64bb783e4c6c77a6
Arti M <artismarti@gmail.com>
2018-09-03 22:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
-  return "I can't hear you!"
+  if (string === "I love you, Grandma.") {
+    return "I love you, too"
   } else if (string === string.toUpperCase) {
     return "YES INDEED!"
-  }
+  } else if (string === string.toLowerCase) {
+    return "I can't hear you!"
 }
