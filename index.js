70e8f9e6e05a7348a2231e7ff3d46a0de8b35f22
Arti M <artismarti@gmail.com>
2018-09-03 22:44:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
